4c35f3004c7e9037c8cc5da021635f1a2d850e30
Ichigo <gmastatine@gmail.com>
2026-08-05 14:46:45 +0600

    Delete Middleware directory

--- Middleware/cors.js
+++
@@ -1,8 +0,0 @@
-const cors = require("cors");
-
-const corsMiddleware = cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"]
-});
-
-module.exports = { corsMiddleware }; // it fuckinggg unwanted fileeeeee
