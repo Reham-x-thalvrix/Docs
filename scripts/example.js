1ebefd8da893f5616a7499ea00bd410484be62e1
Ichigo <gmastatine@gmail.com>
2026-08-05 14:59:16 +0600

    Create example.js

---
+++ scripts/example.js
@@ -0,0 +1,16 @@
+/*
+module.exports = {
+  meta: {
+    name: "Endpoint Name",
+    version: "1.0.0",
+    description: "Brief details",
+    author: "Your Name",
+    method: "get",
+    category: "category-name",
+    path: "/route-path"
+  },
+  async onStart({ req, res }) {
+    // API Controller Logic
+  }
+};
+*/
